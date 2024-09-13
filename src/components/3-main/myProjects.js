2ce2e40d9368484fe10b6e8f2e3efa24a95fcc1a
Mohammed AL-yosofi <your-email@example.com>
2024-09-13 17:16:13 +0300

    first upload by termux

---
+++ src/components/3-main/myProjects.js
@@ -0,0 +1,59 @@
+export const myProjects = [
+  {
+    projectTitle: "react project",
+    category: ["react"],
+    imgPath: "./images/1.jpg",
+    description: "",
+  },
+  {
+    projectTitle: "react & css project",
+    category: ["react", "css"],
+    imgPath: "./images/2.jpg",
+    description: "",
+  },
+  { projectTitle: "css project", category: ["css"], imgPath: "./images/3.jpg" },
+  {
+    projectTitle: "react project",
+    category: ["react"],
+    imgPath: "./images/4.jpg",
+    description: "",
+  },
+  { projectTitle: "css project", category: ["css"], imgPath: "./images/5.jpg" },
+
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/6.jpg",
+    description: "",
+  },
+  {
+    projectTitle: "node project",
+    category: ["node"],
+    imgPath: "./images/7.jpg",
+    description: "",
+  },
+  {
+    projectTitle: "node project",
+    category: ["node"],
+    imgPath: "./images/8.jpg",
+    description: "",
+  },
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/9.jpg",
+    description: "",
+  },
+  {
+    projectTitle:"ecommerce website",
+    category : ['laravel'],
+    imgPath : "",
+    description: "",
+  } ,
+  {
+    projectTitle:"ecommerce website with Tailwindcss",
+    category : ['tailwind'],
+    imgPath : "",
+    description: "",
+  }
+];
